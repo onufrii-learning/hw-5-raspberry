b1a9c8219128a649a8debc34cf01d0769327cf71
onufrii-learning <148785160+onufrii-learning@users.noreply.github.com>
2023-10-23 17:48:13 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-2023/lessons/lesson_5/raspberry/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-2023/lessons/lesson_5/raspberry/' : '/',
+  publicPath: '/'
 })
