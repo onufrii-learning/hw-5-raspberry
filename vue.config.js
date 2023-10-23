4ad4efa23f1c72143a6a061612426e2c8d101d75
Yevhen <onufrii.socnet@gmail.com>
2023-10-23 18:09:29 +0300

    init

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-2023/lessons/lesson_5/raspberry/' : '/',
-  publicPath: '/vue-hw-5-raspberry/',
-})
